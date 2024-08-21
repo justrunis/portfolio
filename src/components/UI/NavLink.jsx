1d78370b93d933dacf466da669d83367f7b003e7
justrunis <justinas.runevicius@ktu.edu>
2024-08-21 17:28:01 +0300

    link bug fix

--- src/components/UI/NavLink.jsx
+++ src/components/UI/NavLink.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 export default function NavLink({ href, children, onClick }) {
   return (
-    <motion.a
-      href={href}
+    <Link
+      to={href}
       className="py-2 px-4 text-xl block lg:inline-block hover:text-secondary focus:text-secondary"
-      whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.95 }}
-      transition={{ duration: 0.3 }}
       onClick={onClick}
     >
       {children}
-    </motion.a>
+    </Link>
   );
 }
